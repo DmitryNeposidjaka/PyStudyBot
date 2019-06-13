59c2f0abce62800223e9de0502a7015a39e975f6
dmitry-neposidyaka <dmitry.neposidyaka@uadevelopers.com>
2019-06-13 21:20:56 +0300

    test task add timer add

--- app/index.py
+++ app/index.py
@@ -1,6 +1,9 @@
 import telebot
 import pymongo
 import json
+from time import sleep
+
+
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 task_by_chat = {}
@@ -22,6 +25,20 @@ class Task:
 
     def to_dict(self):
         return self.__dict__
+
+
+menu_board = ReplyKeyboardMarkup(resize_keyboard=True)
+menu_board.row(
+    KeyboardButton(text='/start ▶️️'),
+    KeyboardButton(text='/add ➕')
+).row(
+    KeyboardButton(text='/users 👥️'),
+    KeyboardButton(text='/test 🧾')
+).row(
+    KeyboardButton(text='/timer ⏱️'),
+    KeyboardButton(text='/contacts ☎️'),
+    KeyboardButton(text='/help ❓️')
+)
 
 
 @bot.message_handler(commands=['start'])
@@ -50,6 +67,17 @@ def start_message(message):
     bot.reply_to(message, commands['help']['data'])
 
 
+@bot.message_handler(commands=['timer'])
+def start_message(message):
+    timer = list('🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚🕛💣💥')
+    message = bot.send_message(message.chat.id, timer[0])
+    for hour in timer[1:]:
+        sleep(1)
+        bot.edit_message_text(hour, chat_id=message.chat.id, message_id=message.message_id)
+    sleep(1)
+    bot.send_message(message.chat.id, 'BUSTED', reply_markup=menu_board)
+
+
 @bot.message_handler(commands=['add'])
 def start_message(message):
     task_by_chat[message.chat.id] = Task()
@@ -75,12 +103,56 @@ def task_finish(message):
     task_by_chat[message.chat.id].comment = message.text
     task_by_chat[message.chat.id].user_id = message.from_user.id
     tasks.insert(task_by_chat[message.chat.id].to_dict())
-    bot.reply_to(message, 'Your task was created')
+    bot.send_message(message.chat.id, 'Your task was created', reply_markup=menu_board)
 
 
 @bot.message_handler(commands=['contacts'])
 def start_message(message):
     bot.reply_to(message, commands['contacts']['data'], parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['test'])
+def start_message(message):
+    testing_categories = ReplyKeyboardMarkup(resize_keyboard=True)
+    testing_categories.row('Basic', 'OOP', 'Other', 'All')
+    message = bot.send_message(message.chat.id, 'Choose category to start testing 📝', reply_markup=testing_categories)
+    bot.register_next_step_handler(message, choose_category)
+
+
+def choose_category(message):
+    category = message.text
+    tasks_count = ReplyKeyboardMarkup(resize_keyboard=True)
+    tasks_count.row('Short (5)', 'Middle (15)', 'Long (30)')
+    message = bot.send_message(message.chat.id, 'How match questions do you want?', reply_markup=tasks_count)
+    bot.register_next_step_handler(message, start_testing, category)
+
+
+def start_testing(message, category):
+    tasks_count_map = {'Short (5)': 5, 'Middle (15)': 5, 'Long (30)': 30}
+    if message.text in tasks_count_map:
+        count = tasks_count_map[message.text]
+        tasks_iterator = tasks.find({"category": category}).limit(count)
+        for task in tasks_iterator:
+            message = bot.send_message(message.chat.id, '{content}'.format(**task))
+
+        #process_task(message, tasks_iterator)
+        #bot.register_next_step_handler(message, process_task, tasks_iterator)
+
+    else:
+        bot.send_message(message.chat.id, 'Only MY variants, you asshole!', reply_markup=menu_board)
+
+
+def process_task(message, tasks_iterator):
+    task_menu = ReplyKeyboardMarkup(resize_keyboard=True)
+    task_menu.row('Stop ⏹', 'Next ▶️️')
+    if message.text is 'Next ▶️️':
+        try:
+            message = bot.send_message(message.chat.id, '{content}'.format(**tasks_iterator.next()), reply_markup=task_menu)
+            bot.register_next_step_handler(message, process_task, tasks_iterator)
+        except:
+            bot.send_message(message.chat.id, 'Finish!', reply_markup=menu_board)
+    elif message.text is 'Stop ⏹':
+        bot.send_message(message.chat.id, 'Come back later)', reply_markup=menu_board)
 
 
 @bot.message_handler(commands=['tasks'])
@@ -96,14 +168,6 @@ def start_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    menu_board = ReplyKeyboardMarkup(resize_keyboard=True)
-    menu_board.row(
-        KeyboardButton(text='/start ▶️️'),
-        KeyboardButton(text='/users 👥️'),
-        KeyboardButton(text='/add ➕'),
-        KeyboardButton(text='/contacts ☎️'),
-        KeyboardButton(text='/help ❓️')
-    )
     bot.send_message(message.chat.id, 'Try functions in menu!', reply_markup=menu_board)
 
 
